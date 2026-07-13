5744faaa43e358f2c5b2aa74c8df412090dd0a94
Kashif Ali <146372614+KashifAli-IT@users.noreply.github.com>
2026-07-13 14:34:21 +0500

    Add numberOfAlternatingGroups method in Solution class
    Implement a sliding window algorithm to count alternating groups of colors.

---
+++ 3206.py
@@ -0,0 +1,17 @@
+# sliding window, two pointers
+class Solution(object):
+    def numberOfAlternatingGroups(self, colors):
+        """
+        :type colors: List[int]
+        :rtype: int
+        """
+        k = 3
+        result = curr = left = 0
+        for right in xrange(len(colors)+k-1):  
+            if right-left+1 == k:
+                result += int(curr == k-1)
+                curr -= int(colors[left] != colors[(left+1)%len(colors)])
+                left += 1
+            curr += int(colors[right%len(colors)] != colors[(right+1)%len(colors)])
+        return result
+
