468655125821f1f5f7e66a6fff34eabffdecade6
Kashif Ali <146372614+KashifAli-IT@users.noreply.github.com>
2026-07-13 14:30:19 +0500

    Implement threeSum function to find triplets

---
+++ 15.py
@@ -0,0 +1,28 @@
+
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = []
+        nums.sort()
+        for i in reversed(xrange(2, len(nums))):
+            if i+1 < len(nums) and nums[i] == nums[i+1]:
+                continue
+            target = -nums[i]
+            left, right = 0, i-1
+            while left < right:
+                if nums[left]+nums[right] < target:
+                    left += 1
+                elif nums[left]+nums[right] > target:
+                    right -= 1
+                else:
+                    result.append([nums[left], nums[right], nums[i]])
+                    left += 1
+                    right -= 1
+                    while left < right and nums[left] == nums[left-1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right+1]:
+                        right -= 1
+        return result
