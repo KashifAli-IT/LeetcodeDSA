88efb42098a806d39b87557627b9e247779f1ec3
Kashif Ali <146372614+KashifAli-IT@users.noreply.github.com>
2026-07-18 21:46:24 +0500

    Implement arrayPairSum method in Solution class

---
+++ 561.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        LEFT, RIGHT = -10000, 10000
+        lookup = [0] * (RIGHT-LEFT+1)
+        for num in nums:
+            lookup[num-LEFT] += 1
+        r, result = 0, 0
+        for i in xrange(LEFT, RIGHT+1):
+            result += (lookup[i-LEFT] + 1 - r) / 2 * i
+            r = (lookup[i-LEFT] + r) % 2
+        return result
