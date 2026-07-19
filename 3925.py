f135d27ffb7b047f2b87aca5fe3d0425f231527d
Kashif Ali <146372614+KashifAli-IT@users.noreply.github.com>
2026-07-19 21:18:09 +0500

    Add method to concatenate list with its reverse

---
+++ 3925.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def concatWithReverse(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        nums.extend(reversed(nums))
+        return nums
